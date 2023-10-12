cceb86a7bb2513e57d2bffbe001f7b327f380637
ainuraiman174 <ainuraiman174>
2023-10-12 15:12:40 +0700

    add belajar JS

---
+++ main.js
@@ -0,0 +1,41 @@
+// //console.log("Hello World")
+// //alert("notifikasi")
+// //prompt("Pinjem seratus")
+
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding not Easy"
+// console,log(Promnet)
+
+// //lst Promnet ="Coding is Easy"
+// //console.log(Promnet)
+// //ist Promnet ="Coding not Easy"
+// //Console.log(Promnet)
+
+// // (x<10 && y > 1);
+// // (x<10 && y < 1);
+// // (x==5||y==5);;
+// // (x==6||y==5);
+
+// // console.log(x<10 && y > 1)
+// // console.log(x<10 && y < 1)
+// // console.log(x==5||y==5)
+// //console.log(x==y))
+
+// let totalPoin = prompt("Masukkan Poin Anda")
+// if(totalPoin>100){
+//     document.write(<h1>"Congratulation"</h1>);
+// }
+// else{
+//     document.write("Thank You");
+// }
+
+// let p = document.querySelector("p")
+// let buttton = document.querySelector("button")
+// let input =document.querySelector("input")
+
+// buttton.addEventListener('click', fumction (){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
